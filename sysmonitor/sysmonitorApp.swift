2d68c991432de43a4d27aff80988cf5f725ccd6a
fanioz <fanio.zilla@gmail.com>
2026-05-29 08:07:01 +0700

    Initial Commit

---
+++ sysmonitor/sysmonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  sysmonitorApp.swift
+//  sysmonitor
+//
+//  Created by fanioz on 29/05/26.
+//
+
+import SwiftUI
+
+@main
+struct sysmonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sysmonitorTests/sysmonitorTests.swift
@@ -0,0 +1,19 @@
+//
+//  sysmonitorTests.swift
+//  sysmonitorTests
+//
+//  Created by fanioz on 29/05/26.
+//
+
+import Testing
+@testable import sysmonitor
+
+struct sysmonitorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
